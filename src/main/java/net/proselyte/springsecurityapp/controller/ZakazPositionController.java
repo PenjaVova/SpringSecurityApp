aaa6cf094f18e159ffada7c8be663064b36e31f3
PenjaVova <penjavladimir@gmail.com>
2017-08-09 17:06:51 +0300

    клиент контроллер добавлено логирование по userName  в классе zakazDoaImpl поменял вызов у сессии метода LOAD на GET

--- src/main/java/net/proselyte/springsecurityapp/controller/ZakazPositionController.java
+++ src/main/java/net/proselyte/springsecurityapp/controller/ZakazPositionController.java
@@ -1,13 +1,17 @@
 package net.proselyte.springsecurityapp.controller;
 
+import net.proselyte.springsecurityapp.model.Client;
 import net.proselyte.springsecurityapp.model.Zakaz;
+import net.proselyte.springsecurityapp.model.ZakazPosition;
 import net.proselyte.springsecurityapp.service.zakaz.ZakazPositionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Vova on 23.07.2017.
@@ -29,5 +33,4 @@ public class ZakazPositionController {
         model.addAttribute("zakazPosition", this.zakazPositionService.getZakazPosition(id));
         return "positionData";
     }
-
 }
